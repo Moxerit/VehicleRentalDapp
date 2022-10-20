2b091ab8f31424ea319cce45ef96b2d2a93ff2a7
Chase Alber <calber207@gmail.com>
2022-10-20 15:57:19 -0600

    dark / light mod theming added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import theme from './theme'
+
+import { ColorModeScript } from '@chakra-ui/react'
 import { BlockchainProvider } from './context/BlockchainContext'
 import { ChakraProvider } from '@chakra-ui/react'
+
+
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BlockchainProvider>
     <ChakraProvider>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <App />
     </ChakraProvider>
   </BlockchainProvider>
